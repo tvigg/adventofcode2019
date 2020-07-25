446d3c75a8c045beb423de907b253b648b01a844
tvigg <tvigg@bahnhof.se>
2020-07-26 00:01:11 +0200

    fsharp version

---
+++ day1/Day1.fs
@@ -0,0 +1,18 @@
+﻿//lots of inspiration from https://github.com/ChrisPritchard/AdventOfCode/blob/master/2019/Day01.fs
+module Day01
+open System.IO
+let input = File.ReadAllLines (@"C:\Users\tvigg\source\repos\adventOfCodeDay1\adventOfCodeDay1\input.txt")
+let part1 = input |> Array.sumBy (float >> fun v -> floor (v / 3.) - 2.) |> int
+
+
+let rec fuel acc x =
+    let res =  floor((float x) / 3.) - 2. |> int
+    match x with
+    | x when res <= 0 -> acc  
+    | x -> fuel  (res + acc) res
+
+let part2 = input |> Array.sumBy (int >> fun v -> fuel 0 v)
+
+
+printfn "%A" part1
+printfn "%A" part2
